34562dda695bed726250d2a461f8f50c2ed87d83
Yanpenghui11 <107158683+Yanpenghui11@users.noreply.github.com>
2022-06-09 08:57:48 +0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+void time(int,int);
+
+int main()
+{
+    int hours;
+    int minutes;
+    cout<<"Enter the number of hours:";
+    cin>>hours;
+    cout<<"Enter the number of minutes:";
+    cin>>minutes;
+    time(hours, minutes);
+    
+    return 0;
+}
+
+void time(int hours,int minutes)
+{
+cout<<"Time: "<<hours<<":"<<minutes<<endl;
+
+}
+    
